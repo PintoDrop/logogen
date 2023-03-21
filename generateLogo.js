7108a70ae49a370443c47b626b32b787915c7cc5
PintoDrop <mikebender22@gmail.com>
2023-03-21 09:39:53 -0700

    few clean ups in code

--- generateLogo.js
+++ generateLogo.js
@@ -38,19 +38,3 @@ module.exports = generateLogo;
 
 // <${renderShape(data.shape)} fill="${data.colorshp}"/>
 
-
-
-// Test code:
-// circle works, square doesn't work unless it 'rect' is listed, triangle does not work
-// triangle = <polygon points="150, 18 244, 182 56, 182"/>
-// square = rect
-
-// <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-//   <rect width="100%" height="100%" fill="red" />
-
-//   <circle cx="150" cy="100" r="80" fill="green" />
-
-//   <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-
-// </svg>
